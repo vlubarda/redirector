71d6e47278053ffb9464e8baa9943b151cf1b6e9
vlubarda <vlubarda@nbty.com>
2014-07-24 13:54:08 -0400

    Initial commit.

---
+++ Controllers/RedirectController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Redirector.Models;
+
+namespace Redirector.Controllers
+{
+    public class RedirectController : Controller
+    {
+        public ActionResult Index()
+        {
+            string url = Request.ServerVariables[ "HTTP_HOST" ]; // "1800-vitamins.net"
+
+            var datacontext = new RedirectDataContext();
+            var redirectUrl = from r in datacontext.Redirects
+                              where r.@base == url
+                              select r.redirect1;
+
+            if ( redirectUrl.Count() > 0 )
+            {
+                return Redirect( redirectUrl.First() );
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
